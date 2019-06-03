1f3f8e7a03a83c9934a0c8cb350da381ab65e0cf
dknj11902 <dankim20@bergen.org>
2019-06-03 15:07:19 -0400

    updated play_gui.py

--- play_gui.py
+++ play_gui.py
@@ -4,22 +4,22 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
 
 class Application(Frame):
-    def __init__(self, master, width, height):
+    def __init__(self, master):
         super(Application, self).__init__(master)
         self.grid()
         self.master = master
-        self.canvas = Canvas(master, width=width, height=height)
+        self.welcome()
 
     def welcome(self):
         self.title = Label(self,
               text = "BS Poker")
         self.title.config(font = ("Helvetica", 35))
-        self.title.grid(row = 0, column = 0, columnspan = 3)
+        self.title.grid(row = 0, column = 0, columnspan = 3, padx = 25, pady = 20)
         self.start_btn = Button(self,
                                 text = "Play",
                                 command = self.set_up_board)
-        self.start_btn.config(font = ("Helvetica", 18))
-        self.start_btn.grid(row = 1, column = 2)
+        self.start_btn.config(font = ("Helvetica", 18), pady = 20)
+        self.start_btn.grid(row = 1, column = 1)
 
     def set_up_board(self):
         print("")
@@ -29,7 +29,7 @@ def main():
     root = Tk()
     root.title("BS Poker")
     # root.geometry(str(WINDOW_WIDTH)+"x"+str(WINDOW_HEIGHT))
-    app = Application(root, WINDOW_WIDTH, WINDOW_HEIGHT)
+    app = Application(root).pack(fill = BOTH, expand=True)
     root.mainloop()
 
 main()
