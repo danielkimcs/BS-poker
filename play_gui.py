5f261c52ecb3c39ecb39f57b7cf2caa92e33c3dc
dknj11902 <dankim20@bergen.org>
2019-06-03 14:23:03 -0400

    started play_gui.py

---
+++ play_gui.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+
+class Application(Frame):
+    def __init__(self, master, width, height):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.master = master
+        self.canvas = Canvas(master, width=width, height=height)
+
+    def welcome(self):
+        self.title = Label(self,
+              text = "BS Poker")
+        self.title.config(font = ("Helvetica", 35))
+        self.title.grid(row = 0, column = 0, columnspan = 3)
+        self.start_btn = Button(self,
+                                text = "Play",
+                                command = self.set_up_board)
+        self.start_btn.config(font = ("Helvetica", 18))
+        self.start_btn.grid(row = 1, column = 2)
+
+    def set_up_board(self):
+        print("")
+
+
+def main():
+    root = Tk()
+    root.title("BS Poker")
+    # root.geometry(str(WINDOW_WIDTH)+"x"+str(WINDOW_HEIGHT))
+    app = Application(root, WINDOW_WIDTH, WINDOW_HEIGHT)
+    root.mainloop()
+
+main()
