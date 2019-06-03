fa64b9b0a9897b3dca7f522b7a80ea1b39694b83
Ethan Herbst <ethher20@bergen.org>
2019-06-03 15:04:41 -0400

    Removed bets, there are none in BS Poker

--- Player.py
+++ Player.py
@@ -1,10 +1,9 @@
-from BSPokerProject.Card import Card
+from Card import *
 
 class Player(object):
     def __init__(self, num=1):
         self.num = num
         self.cards = []
-        self.bet = None
 
     def increment(self):
         self.num += 1
@@ -21,10 +20,4 @@ class Player(object):
     def add_card(self, card):
         assert isinstance(card, Card)
         self.cards.append(card)
-
-    def set_bet(self, bet):
-        self.bet = bet
-
-    def get_bet(self):
-        return self.bet
 
