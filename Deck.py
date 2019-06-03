39cc11eebea722252c9e0c95cfb2c8582044f688
Ethan Herbst <ethher20@bergen.org>
2019-06-03 14:21:40 -0400

    list of cards with functions for it

---
+++ Deck.py
@@ -0,0 +1,20 @@
+from Card import *
+from random import *
+
+
+class Deck(object):
+
+    def __init__(self):
+        self.deck = []
+        for num in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
+            for suit in ["S", "H", "C", "D"]:
+                self.deck.append(Card(num, suit))
+
+    def shuffle(self):
+        shuffle(self.deck)
+    
+    def remove_card(self, card):
+        return self.deck.pop(0)
+
+    def add_card(self, card):
+        self.deck.append(card)
