fb3b0710ca98ce29e73c877c7bb1998d77a033ed
Jazhang01 <jazhang919@gmail.com>
2019-06-05 19:51:55 +0100

    tester file for Deck.commit

--- tester.py
+++ tester.py
@@ -1,9 +1,37 @@
-from BSPokerProject.Card import Card
+from Card import Card
+from Deck import Deck
+from Player import Player
+from Table import Table
 
-c1 = Card(2, "S")
-c2 = Card(2, "S")
-c3 = Card(3, "S")
+t = Table(5)
+for i in range(20):
+    p = Player()
+    t.add_player(p)
 
-print(c1 == c2)
-print(c2 <= c3)
-print(c2 >= c3)
+t.distribute_cards()
+for i in t.deck.deck:
+    print(i)
+
+print("test single" + "-"*10)
+for i in range(3,15):
+    print(t.contains("single", i, count_bound=5))
+
+print("test straight" + "-"*10)
+for i in range(7,15):
+    print(t.contains("five", rank_bound=i, rank_bound2=i-4, count_bound=5))
+
+print("test flush" + "-"*10)
+for i in range(7,15):
+    for suit in ["S", "H", "C", "D"]:
+        print(t.contains("five", rank_bound=i, count_bound=5, suit_bound=suit), end=", ")
+    print()
+
+print("test full house" + "-"*10)
+for i in range(3,15):
+    print(t.contains("full house", rank_bound=i))
+
+print("test two pair" + "-"*10)
+for i in range(3,15):
+    for j in range(3, 15):
+        if i != j:
+            print(i, ":", j, "::", t.contains("double", rank_bound=i, rank_bound2=j))
